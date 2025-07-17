8140a07c9e981e927011a2754603dd683633f6df
Axel Jhuan Solis Zamata <104176510+AxelSolis93@users.noreply.github.com>
2025-07-16 19:18:36 -0500

    Now downloads LjSpeech Dataset

--- examples/LjSpeech/cosyvoice2/local/download_and_untar.sh
+++ examples/LjSpeech/cosyvoice2/local/download_and_untar.sh
@@ -20,78 +20,91 @@ if [ $# -ne 3 ]; then
 fi
 
 data=$1
-url=$2
-part=$3
+url="https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
+part="LJSpeech-1.1"
 
 if [ ! -d "$data" ]; then
   echo "$0: no such directory $data"
   exit 1
 fi
 
-part_ok=false
-list="dev-clean test-clean dev-other test-other train-clean-100 train-clean-360 train-other-500"
-for x in $list; do
-  if [ "$part" == $x ]; then part_ok=true; fi
-done
-if ! $part_ok; then
-  echo "$0: expected <corpus-part> to be one of $list, but got '$part'"
+#part_ok=false
+#list="dev-clean test-clean dev-other test-other train-clean-100 train-clean-360 train-other-500"
+#for x in $list; do
+#  if [ "$part" == $x ]; then part_ok=true; fi
+#done
+#if ! $part_ok; then
+#  echo "$0: expected <corpus-part> to be one of $list, but got '$part'"
+#  exit 1
+#fi
+#
+#if [ -z "$url" ]; then
+#  echo "$0: empty URL base."
+#  exit 1
+#fi
+#
+#if [ -f $data/LibriTTS/$part/.complete ]; then
+#  echo "$0: data part $part was already successfully extracted, nothing to do."
+#  exit 0
+#fi
+
+if [ ! -d "$data" ]; then
+  echo "$0: no such directory $data"
   exit 1
 fi
 
-if [ -z "$url" ]; then
-  echo "$0: empty URL base."
-  exit 1
-fi
-
-if [ -f $data/LibriTTS/$part/.complete ]; then
+if [ -f $data/$part/.complete ]; then
   echo "$0: data part $part was already successfully extracted, nothing to do."
   exit 0
 fi
 
 
 # sizes of the archive files in bytes.  This is some older versions.
-sizes_old="371012589 347390293 379743611 361838298 6420417880 23082659865 30626749128"
+#sizes_old="371012589 347390293 379743611 361838298 6420417880 23082659865 30626749128"
 # sizes_new is the archive file sizes of the final release.  Some of these sizes are of
 # things we probably won't download.
-sizes_new="337926286 314305928 695964615 297279345 87960560420 33373768 346663984 328757843 6387309499 23049477885 30593501606"
+#sizes_new="337926286 314305928 695964615 297279345 87960560420 33373768 346663984 328757843 6387309499 23049477885 30593501606"
 
-if [ -f $data/$part.tar.gz ]; then
-  size=$(/bin/ls -l $data/$part.tar.gz | awk '{print $5}')
-  size_ok=false
-  for s in $sizes_old $sizes_new; do if [ $s == $size ]; then size_ok=true; fi; done
-  if ! $size_ok; then
-    echo "$0: removing existing file $data/$part.tar.gz because its size in bytes $size"
-    echo "does not equal the size of one of the archives."
-    rm $data/$part.tar.gz
+size_ljspeech="2744553444" 
+
+
+if [ -f $data/$part.tar.bz2 ]; then
+  size=$(/bin/ls -l $data/$part.tar.bz2 | awk '{print $5}')
+  if [ "$size" != "$size_ljspeech" ]; then
+    echo "$0: removing existing file $data/$part.tar.bz2 because its size in bytes $size"
+    echo "does not equal the expected size of $size_ljspeech."
+    rm $data/$part.tar.bz2
   else
-    echo "$data/$part.tar.gz exists and appears to be complete."
+    echo "$data/$part.tar.bz2 exists and appears to be complete."
   fi
 fi
 
-if [ ! -f $data/$part.tar.gz ]; then
+
+if [ ! -f $data/$part.tar.bz2 ]; then
   if ! which wget >/dev/null; then
     echo "$0: wget is not installed."
     exit 1
   fi
-  full_url=$url/$part.tar.gz
-  echo "$0: downloading data from $full_url.  This may take some time, please be patient."
+  echo "$0: downloading data from $url. This may take some time, please be patient."
 
-  if ! wget -P $data --no-check-certificate $full_url; then
-    echo "$0: error executing wget $full_url"
+  if ! wget -P $data --no-check-certificate $url; then
+    echo "$0: error executing wget $url"
     exit 1
   fi
 fi
 
-if ! tar -C $data -xvzf $data/$part.tar.gz; then
-  echo "$0: error un-tarring archive $data/$part.tar.gz"
+if ! tar -C $data -xvf $data/$part.tar.bz2; then
+  echo "$0: error un-tarring archive $data/$part.tar.bz2"
   exit 1
 fi
 
-touch $data/LibriTTS/$part/.complete
+touch $data/$part/.complete
 
-echo "$0: Successfully downloaded and un-tarred $data/$part.tar.gz"
+
+#echo "$0: Successfully downloaded and un-tarred $data/$part.tar.gz"
+echo "$0: Successfully downloaded and un-tarred $data/$part.tar.bz2"
 
 if $remove_archive; then
-  echo "$0: removing $data/$part.tar.gz file since --remove-archive option was supplied."
-  rm $data/$part.tar.gz
+  echo "$0: removing $data/$part.tar.bz2 file since --remove-archive option was supplied."
+  rm $data/$part.tar.bz2
 fi
